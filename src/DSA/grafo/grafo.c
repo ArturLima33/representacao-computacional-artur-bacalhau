e93d53cb867f8d08909b2e75e3fe203b6cb8ee6b
ArturLima33 <169162600+ArturLima33@users.noreply.github.com>
2026-09-14 23:50:15 +0000

    Implementacao da atividade

---
+++ src/DSA/grafo/grafo.c
@@ -0,0 +1,47 @@
+#include "grafo.h"
+#include <stdlib.h>
+
+Grafo *criarGrafo() {
+    Grafo *g = (Grafo *)malloc(sizeof(Grafo));
+
+    if (g == NULL)
+        return NULL;
+
+    inicializarListaAdj(g->listaAdj);
+    inicializarMatrizAdj(g->matrizAdj);
+
+    return g;
+}
+
+bool inserirAresta(Grafo *g, int origem, int destino) {
+    if (g == NULL)
+        return false;
+
+    if (!inserirArestaMatriz(g->matrizAdj, origem, destino))
+        return false;
+
+    No *novo = criarNo(destino);
+
+    if (novo == NULL)
+        return false;
+
+    novo->proximo = g->listaAdj[origem];
+    g->listaAdj[origem] = novo;
+
+    return true;
+}
+
+bool existeAresta(Grafo *g, int origem, int destino) {
+    if (g == NULL)
+        return false;
+
+    return existeArestaMatriz(g->matrizAdj, origem, destino);
+}
+
+void destruirGrafo(Grafo *g) {
+    if (g == NULL)
+        return;
+
+    destruirListaAdj(g->listaAdj);
+    free(g);
+}
